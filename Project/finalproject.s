39f852af6c94d810c683f9987d334bfe25d21c5b
hu-simon <jhhu@ucsd.edu>
2017-03-14 01:34:58 -0700

    Finishing the rest of it tomorrow

--- Project/finalproject.s
+++ Project/finalproject.s
@@ -53,49 +53,72 @@ MaxSumBoundary:
 #	$a3 is the direction (either 0 or 1)
 #	$v0 returns the maximum subarray
 #   Write your code here
-addi $sp, $sp, 32	# increment the stack pointer by 32 words
-sw   $ra, 0($sp)	# store old return address
-sw   $fp, 4($sp)	# store old frame pointer
-sw   $a1, 8($sp)	# store the value of s
-sw   $a2, 12($sp) 	# store the value of e
-sw   $a3, 16($sp)	# store the direction value
-sw   $v0, 20($sp) 	# store the return value
-jal calculate
-sw   $s0, 24($sp) 	# store the value in the array at the current array position
-addi $fp, $fp, 32	# increment the frame pointer
-execute:
-beq $a1, $a2, base_case	# check for base case, s == e
-beq $a3, $0, backwards  # if $a3 == 0 then traverse backwards, otherwise, traverse forwards
+addi $sp, $sp, -32	# move the stack pointer up
+sw $ra, 4($sp)		# store return address
+sw $fp, 8($sp) 		# store frame pointer
+sw $s0, 12($sp)		# store the save values onto the stack
+sw $s1, 16($sp)		
+sw $s2, 20($sp)	
+addi $fp, $sp, 32	# move the frame pointer up
+
+beq $a1, $a2, baseCase  # if s == e, then go to notEq. otherwise, base case
+
+notEqual:		# check for the direction 
+beq $a3, $0, backwards	# if $a3 == 0, traverse backwards
+j forwards		# otherwise, $a3 != 0, traverse forwards
+
+baseCase:
+sll $t0, $a1, 2		# multiply s by four
+add $t1, $a0, $t0	# set $t1 = arr + (s*4)
+lw $v0, 0($t2) 		# return value of arr[s]. i.e. store it in output register
+j MaxSumBoundaryEnd	# jump to the end
+
 forwards:
+addi $sp, $sp, -32 	# allocate space for the stack. move $sp up
+sw $a0, 4($sp)		# save arguments on the stack
+sw $a1, 8($sp) 	
+sw $a2, 12($sp)
+sw $a3, 16($sp)
+
+addi $a0, $a0, 0	# set parameters, prepare for recursive call
 addi $a1, $a1, 1	# increment s by 1
-jal MaxSumBoundary	# call MaxSumBoundary on s + 1
-jal base_case
-jr $ra
+addi $a2, $a2, 0	# e does not change
+addi $a3, $0, 1		# direction is still 1 (traversing to the right)
+jal MaxSumBoundary	# recursive call
+			# now we are done with the recursive call and we are back 
+lw $a0, 4($sp)		# restore the arguments
+lw $a1, 8($sp)	
+lw $a2, 12($sp)
+lw $a3, 16($sp)
+addi $sp, $sp, 32	# move the stack pointer down, prepare to pop stack out
+addi $s0, $v0, 0	# store the output in register $s0
+
+addi $sp, $sp, -32	# move stack pointer up, prepare another stack
+sw $a0, 4($sp)		# store the argument registers onto the stack
+sw $a1, 8($sp) 	
+sw $a2, 12($sp)
+sw $a3, 16($sp)
+
+sll $t0, $a2, 2		# multiply s by 4
+add $t1, $a0, $t0	# calculate arr + (s * 4)
+lw $t2, 0($t1)		# store arr[s] into register $t2
+add $t3, $t2, $s0	# store into $t3, arr[s] + the previous value that was popped out
+addi $a1, $t2, 0	# set first argument for FindMax2 procedure
+addi $a2, $t3, 0	# set second argument for FindMax2 procedure, prepare to jump
+jal FindMax2		# jump to the FindMax2 procedure, find maximum of two arguments
+			# now we have found the maximum and it is stored in $v0. we proceed to restore arguments
+lw $a0, 4($sp) 		#restore arguments
+lw $a1, 8($sp)
+lw $a2, 12($sp)	
+lw $a3, 16($sp)
+addi $sp, $sp, 32	# move the stack pointer down, push the stack out
+addi $v0, $v0, 0	# store the output in $v0 register
+j MaxSumBoundaryEnd	# jump to the end, we are finished
+
 backwards:
-addi $a2, $a2, -1 	# decrement e by 1
-jal MaxSumBoundary	# call MaxSumBoundary on e - 1
-jal base_case
-jr $ra
-
-base_case:		# we are now at the base case
-jal calculate 		# calculate the current array value (base case value)
-add $s1, $0, $s0	# store this in $s1
-add $s2, $s1, $s0	# add the two elements
-add $a1, $0, $s1	# load the first number into the argument
-add $a2, $0, $s2	# load the second number into the argument
-jal FindMax2 		# call the FindMax2 procedure
-sw $v0, 28($sp)		# store the result of FindMax2
-lw $ra, 0($sp)	
-lw $fp, 4($sp)	
-jr $ra
-
-calculate:
-sll $s0, $a1, 2		# multiply s by four
-add $s0, $s0, $a0	# add the address of s to the address of the first element of array
-lw $s0, 0($s0)		# load arr[s]
-add $s0	$0, $s0		# place arr[s] into register $t1
-jr $ra
+# To implement tomorrow.  
 ##########################################################
+
 MaximumCrossingSum:
 #	$a0 contains arr[]
 #	$a1 contains s
